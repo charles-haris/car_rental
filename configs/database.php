aa30bcc1c16c91e0badbf2aa2ff8dce6c611737a
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-02-16 23:37:24 +0000

    Update database.php

--- configs/database.php
+++ configs/database.php
@@ -8,7 +8,6 @@ $password = "root";
 try {
   //$databaseConnexion = new PDO("mysql:host=$servername;dbname=car_rental", $username, $password);
   $databaseConnexion = new PDO('pgsql:host=localhost;port=5432;dbname=car_rental;user=postgres;password=passer123');
-
   // set the PDO error mode to exception
   $databaseConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully"; 
